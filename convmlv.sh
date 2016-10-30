7a188bc2b5f59e4e0dd37c70611865472106c3e0
Sofus Rose <sofus@sofusrose.com>
2016-10-29 23:00:47 -0400

    Refocused planning section.

--- convmlv.sh
+++ convmlv.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
 #TODO:
+
+#PLANNING:
+#	Except for openlut integration, CM fixes, and the last throes of Mac compatibility, I consider convmlv feature-complete. As in, only bug fixes and error checking.
+#	New developments, like special ffmpeg filters, or new techniques from the ML world, may change that. But that's nothing compared to the 2400 lines this pushes at the moment!
+#	Any further effort is better focused on compatibility, bug fixes, error checking, more bug fixes, and finally a GUI app described in FUTURE.
+
+
+#~ TOP PRIORITY
+#~ --> Fix Color Management; -o 5 is broken. Use -o 0, supplemented by current paradigm of Rec709 -> Whatever LUTs, instead of XYZ -> Whatever LUTs.
+
 #~ HIGH PRIORITY
 #~ --> Retest Darkframe subtraction with preaveraged/naked darkframe MLVs.
-#~ --> More LUTs - ACES, LogC
+#~ --> Integrate openlut for 1D LUTs. 3D LUTs would only be used for gamut transforms, not gamut/gamma.
 #~ --> More error checking.
-#~ --> Flawless Mac Compatibility.
+#~ --> Test Mac compatibility with a *working* (with 10.7) mlv_dump...
 #~ --> Documentation: PDF and Videos.
 
 #~ MEDIUM PRIORITY
-#~ --> HALD LUT support. If possible. If not, whatever.
-#~ --> Stats for .RAW files.
+#~ --> Integrate openlut for gamut ops. Matrices would replace standard 3D LUTs altogether, and openlut would handle 3D LUTs.
 
 #~ LOW
-#~ --> A GUI with a preview window, which runs convmlv on-demand.
-#~ --> More WB Algorithms
-#~ --> Windows 10 Support
+#~ --> Stats for .RAW files.
 
-#BUG: Relative OUTDIR makes baxpixel generation fail if ./mlv2badpixels.sh doesn't exist. Fixed on Linux only.
-#CONCERN: Weirdness with color spaces in general. No impact to user; just some weirdly placed sRGB conversions if'ed by DPX.
+
+#~ FUTURE
+#~ --> A GUI app, running libraw variants, which has a CLI backend, but can also output a convmlv config file.
+#~     --> convmlv will always have more features - bash makes certain nasty things like ffmpeg filters available, as opposed to the "code it yourself" of true image array processing...
+#~     --> But, a GUI is usable by the average non-nerd!! You know, the users of ML.
+
+
+#BUG: Relative OUTDIR makes baxpixel generation fail if ./mlv2badpixels.sh doesn't exist. Should be fixed on all platforms.
+#BIG BUG: Color Management is oversaturated, destructively so.
 #~ --> See Nasty Hacks Part 1 - 3. FML :O.
-#~ I'll feel better about this after quite a while
-
 
 #~ The MIT License (MIT)
 
@@ -1676,7 +1688,7 @@ for ARG in "${FILE_ARGS_ITER[@]}"; do #Go through FILE_ARGS_ITER array, copied f
 	if [[ $OSTYPE == "linux-gnu" ]]; then
 		OUTDIR="$(readlink -f $OUTDIR)"  >/dev/null 2>/dev/null #Relative Badpixel OUTDIR fixed properly on Linux.
 	elif [[ $OSTYPE == "darwin11" ]]; then
-		ARG="$(readlinkFMac $OUTDIR)"  >/dev/null 2>/dev/null #Mac relative OUTDIR uses the special readlinkFMac :)
+		OUTDIR="$(readlinkFMac $OUTDIR)"  >/dev/null 2>/dev/null #Mac relative OUTDIR uses the special readlinkFMac :)
 	fi
 	
 	if [ $OUTDIR != $PWD ] && [ $isOutGen == false ]; then
